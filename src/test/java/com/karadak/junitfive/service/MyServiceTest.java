92a10583b3ac4e9ea31dd8b7a97d8d61d01f79fb
onelsonreyes <onelson.reyes@gmail.com>
2017-07-31 14:30:01 -0600

    Added test cases to capture exception and all asertions

--- src/test/java/com/karadak/junitfive/service/MyServiceTest.java
+++ src/test/java/com/karadak/junitfive/service/MyServiceTest.java
@@ -1,6 +1,7 @@
 package com.karadak.junitfive.service;
 
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
@@ -9,7 +10,11 @@ import java.util.List;
 import static java.util.Arrays.asList;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.core.Is.is;
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class MyServiceTest {
 
@@ -18,14 +23,29 @@ public class MyServiceTest {
     @ParameterizedTest
     @ValueSource(strings = {"Hello", "World"})
     @DisplayName("fail when wrong messages are provided")
-    void contains_message_AssertionError(String message) {
+    public void contains_message_AssertionError(String message) {
         assertFalse(VALID_MESSAGES.contains(message), "contains a valid message");
     }
 
     @ParameterizedTest
     @ValueSource(strings = {"message 1", "message 2"})
     @DisplayName("correct messages")
-    void contains_message_correct(String message) {
+    public void contains_message_Correct(String message) {
         assertThat(VALID_MESSAGES.contains(message), is(true));
     }
+
+    @Test
+    @DisplayName("assert with All 3 test cases")
+    public void contains_validMessages_assertAll_Correct() {
+        assertAll(() -> assertNotEquals("not a valid message", VALID_MESSAGES.get(0)),
+                () -> assertEquals("message 1", VALID_MESSAGES.get(0)),
+                () -> assertEquals("message 2", VALID_MESSAGES.get(1)));
+    }
+
+    @Test
+    @DisplayName("Fail to get a message when index is wrong")
+    public void get_validMessage_ThrowException() throws Exception {
+        assertThrows(ArrayIndexOutOfBoundsException.class, () -> VALID_MESSAGES.get(666));
+    }
+
 }
